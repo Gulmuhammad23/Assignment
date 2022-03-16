a1f516ca564cddd1f5c64b49072561b8b5c0412e
Gulmuhammad23 <100403996+Gulmuhammad23@users.noreply.github.com>
2022-03-16 20:02:10 +0500

    Add files via upload

---
+++ Assignment3.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+
+void main() {
+  // task1
+  // stdout.write("Enter first subject.");
+  // var fsubject = stdin.readLineSync();
+  // stdout.write("Enter scount subject.");
+  // var ssubject = stdin.readLineSync();
+  // stdout.write("Enter third subject.");
+  // var tsubject = stdin.readLineSync();
+
+  // var totalmarksforeachsubject = 100;
+  // stdout.write("Enter first subjec number.");
+  // var obtfor1stsubject = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter scound subject number.");
+  // var obtfor2ndsubject = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter third subject number.");
+  // var obtfor3rdsubject = int.parse(stdin.readLineSync()!);
+  // print("Obtain total marks ${obtfor1stsubject+obtfor2ndsubject+obtfor3rdsubject}");
+  // print("Percentage  => ${(obtfor1stsubject+obtfor2ndsubject+obtfor3rdsubject)/(totalmarksforeachsubject*3)*100} Perscent");
+
+  // task2
+// stdout.write("Enter name of city.");
+// var c = stdin.readLineSync()!;
+// c.toLowerCase();
+// (c=="karachi")
+//   ? print("welcome to city of light")
+//   :print("Enter karachi.");
+
+// task 3
+// stdout.write("Enter your gender.");
+// var g = stdin.readLineSync()!;
+// g.toLowerCase();
+// if
+// (g=='m' || g=='Male' || g=='MaLe' || g=='MALE' || g=='male' || g=='MALe'){
+// print("Good morning sir.");
+// } else if (g=='f' || g=='FM' || g=="fm" || g=='Female' || g=='FEMaLe' || g=='FEMALE' || g=='female' || g=='FeMALe') {
+//   print("Good morning ma'am");
+// }
+// task4
+// stdout.write("what is the right now in car.");
+// var frange = double.parse(stdin.readLineSync()!);
+// (frange < 0.25)? print("Please Refiel sir.") : print("Good to go.");
+
+// task5
+// stdout.write("Enter your city tem.");
+// var temp = int.parse(stdin.readLineSync()!);
+// if (temp>40){
+// print("It is to hot today in your city.");
+// }
+// else if (temp>30){
+// print("Today weather is normal.");
+// }
+// else if (temp>20){
+//   print("Today weather is cool.");
+// }
+// else if (temp>10){
+//   print("OMG! today weather is so cool");
+// }
+// else{
+// print("Please enter correct value");}
+// print("Please enter correct value.");
+
+// task6
+// stdout.write("Enter Number or word.");
+// var nw=stdin.readByteSync();
+// var a=stdin.readByteSync();
+// if (a>=65&&a<=90){
+// print("Upper case");
+// }
+// if (a>=97&&a<=122){
+//   print("Lower case");
+// }
+// else if ( a >= 48 || a <= 57){
+// print("Number");
+// }
+// task7
+  // stdout.write("Enter first number.");
+  // int num1 = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter scound number.");
+  // int num2 = int.parse(stdin.readLineSync()!);
+  // if (num1 > num2) {
+  //   print("num1 is greater.");
+  // } else {
+  //   print("num2 is greater.");
+  // }
+}
